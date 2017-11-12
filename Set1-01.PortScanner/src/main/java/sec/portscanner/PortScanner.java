0c4df5c868846927edb92e8e761fd42314713859
Yuce Hamit <hamit.yuce@aalto.fi>
2017-11-12 22:18:15 +0200

    port scanner

---
+++ Set1-01.PortScanner/src/main/java/sec/portscanner/PortScanner.java
@@ -0,0 +1,52 @@
+package sec.portscanner;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PortScanner {
+
+    final static int MIN_PORT = 1024;
+    final static int MAX_PORT = 49151;
+
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Which address should I scan?");
+        String address = reader.nextLine();
+        System.out.println("Start at port?");
+        int start = Integer.parseInt(reader.nextLine());
+        System.out.println("End at port?");
+        int end = Integer.parseInt(reader.nextLine());
+
+        Set<Integer> ports = getAccessiblePorts(address, start, end);
+        System.out.println("");
+
+        if (ports.isEmpty()) {
+            System.out.println("None found :(");
+        } else {
+            System.out.println("Found:");
+            ports.stream().forEach(p -> System.out.println("\t" + p));
+        }
+    }
+
+    public static Set<Integer> getAccessiblePorts(String address, int start, int end) {
+        Set<Integer> accessiblePorts = new TreeSet<>();
+        start = Math.max(start, MIN_PORT);
+        end = Math.min(end, MAX_PORT);
+        for (int port = start; port <= end; port++) {
+             try {
+                Socket socket = new Socket(address,port);
+               // socket.connect(new SocketAddress() {}, 1000);
+                socket.close();
+                System.out.println("Port " + port + " is open");
+                accessiblePorts.add(port);
+            } catch (Exception ex) {
+            }
+         }
+         return accessiblePorts;
+    }
+}
